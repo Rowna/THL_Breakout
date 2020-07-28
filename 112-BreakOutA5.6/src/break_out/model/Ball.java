8cb20ed228e852d1dbc7c9a434a98975e8321449
Rowna <rowan.kh1@gmail.com>
2020-07-28 14:05:31 +0200

    Kommentare übersetzt

--- 112-BreakOutA5.6/src/break_out/model/Ball.java
+++ 112-BreakOutA5.6/src/break_out/model/Ball.java
@@ -5,59 +5,66 @@ import break_out.model.Position;
 import java.awt.Rectangle;
 
 /**
- * This class contains the information about the balls characteristics and behavior
+ * Diese Klasse enthält alle Daten über die Eigenschaften und Actionen des Balls
  * 
- * @author iSchumacher modified by Saman Shayanfar, Bashar Alsamar
+ * @author
  *
  */
 public class Ball {
 
 	/**
-	 * The balls position on the playground
+	 * Position auf dem Spielfeld
 	 */
 	private Position position;
 	
 	/**
-	 * The balls direction
+	 * Bewegungsrichtung des Balls
 	 */
 	private Vector2D direction;
 	
 	
 	/**
-	 * Objektvariable hitStonePosition vom Datentyp MatrixPosition
+	 * MatrixPosition: Wo wurde der letzte Stein getroffen?
 	 */
 	private MatrixPosition hitStonePosition;
 	
 	/**
-	 * The constructor of a ball
-	 * The balls position and direction are initialized here.
+	 *  Konstruktor
+	 *  
+	 *  Initialisiert Position und Bewegungsrichtung des Balls
+	 *  
 	 */
 	public Ball() {
 		this.position = new Position(Constants.SCREEN_WIDTH / 2 - Constants.BALL_DIAMETER / 2  , 
 				Constants.SCREEN_HEIGHT - Constants.BALL_DIAMETER - Constants.PADDLE_HEIGHT);	
 		this.direction = new Vector2D(5,-5);
-		// Normieren der Richtung mit anpasung der Geschwindigkeit
+		
+		// Neueinstellung der Bewegungsrichtung und Anpassung der Geschwindigkeit
 		direction.rescale();
 	}
 	
 	/**
-	 * The getter for the balls position
-	 * @return position The balls current position
+	 * Getter für die aktuelle Position
+	 *  
+	 * @return position Die aktuelle Position des Balls 
+	 * 					
 	 */
 	public Position getPosition() {
 		return this.position;
 	}
 	
 	/**
-	 * The getter for the balls direction
-	 * @return direction The balls current direction
+	 * Getter für die Bewegungsrichtung
+	 * 
+	 * @return direction Die aktuelle Bewegungsrichtung des Balls 
+	 * 					
 	 */
 	public Vector2D getDirection() {
 		return this.direction;
 	}
 	
 	/**
-	 * Der getter fuer die Position des Balls in der Steinmatrix
+	 * Getter für die Position des Balls innerhalb der Stein-Matrix
 	 * @return hitStonePosition 
 	 */
 	public MatrixPosition getHitStonePosition() {
@@ -68,50 +75,52 @@ public class Ball {
 	
 	/**
 	 * Methode um die neue Position des Balls zu ermitteln
-	 * unter benutzung der alten Position und der Richtungsaenderung
+	 * (unter Benutzung der alten Position und der Richtungsaenderung)
 	 */
 	public void updatePosition() {
-		// Ermittelt die neue x Koordinate des Balls ueber Aufruf der set Methode
-		// Ihr wird die alte Ballposition (get) und die Richtung uebergeben
+		// Ermittelt die neue X-Koordinate des Balls durch Aufruf der setX()-Methode
+		// Ihr wird die alte Ballposition auf der X-Achse (getX) 
+		// und die horizontale Bewegungsrichtung uebergeben
   	  	position.setX (position.getX() + direction.getDx());
-  	  	// Ermittelt die neue y Koordinate des Balls ueber Aufruf der set Methode
-  		// Ihr wird die alte Ballposition (get) und die Richtung uebergeben
+		// Ermittelt die neue Y-Koordinate des Balls durch Aufruf der setY()-Methode
+		// Ihr wird die alte Ballposition auf der Y-Achse (getY) 
+		// und die vertikale Bewegungsrichtung uebergeben
   	  	position.setY (position.getY() + direction.getDy());
 	}
 	
 	/**
-	 * Methode um das verhalten des balls bei Wandberuehrung zu prufen
+	 * Methode, um das Verhalten des Balls bei Wand- oder Steinberuehrung zu steuern
 	 */
 	public void reactOnBorder() {
     	// rechte Wandberuehrung pruefen
     	if (position.getX() >= Constants.SCREEN_WIDTH - Constants.BALL_DIAMETER ) {
-    		// Setze auf Randberuehrung
+    		// -> Neue X-Position
     		position.setX(Constants.SCREEN_WIDTH - Constants.BALL_DIAMETER);
-    		// Richtung negieren (Einfallswinkel = Ausfallswinkel)
+    		// -> Neue X-Bewegungsrichtung (Einfallswinkel == Ausfallswinkel)
     		direction.setDx(-direction.getDx());
     	}
     	
     	// linke Wandberuehrung pruefen
     	if (position.getX() <= 0) {
-    		// Setze auf Randberuehrung
+    		// Neue X-Position festlegen
     		position.setX(0);
-    		// Richtung negieren (Einfallswinkel = Ausfallswinkel)
+    		// Richtung negieren (Einfallswinkel == Ausfallswinkel)
     		direction.setDx(-direction.getDx());
     	}
     	
     	// obere Wandberuehrung pruefen
     	if (position.getY() <= 0) {
-    		// Setze auf Randberuehrung
+    		// Neue Y-Position festlegen
     		position.setY(0);
-    		// Richtung negieren (Einfallswinkel = Ausfallswinkel)
+    		// Richtung negieren (Einfallswinkel == Ausfallswinkel)
     		direction.setDy(-direction.getDy());
     	}
     	
     	// untere Wandberuehrung pruefen
     	if (position.getY() >= Constants.SCREEN_HEIGHT - Constants.BALL_DIAMETER) {
-    		// Setze auf Randberuehrung
+    		// Neue Y-Position festlegen
     		position.setY(Constants.SCREEN_HEIGHT - Constants.BALL_DIAMETER);
-    		// Richtung negieren (Einfallswinkel = Ausfallswinkel)
+    		// Richtung negieren (Einfallswinkel == Ausfallswinkel)
     		direction.setDy(-direction.getDy());
     	}
     	
@@ -119,9 +128,9 @@ public class Ball {
 	
 	
 	/**
-	 * Methode um zu testen ob der ball die obere seite des Paddles beruehrt
-	 * @param p Eine Variable des datentyp Paddle
-	 * @return true nach treffen des Paddles
+	 * Methode um zu testen, ob der Ball die obere Seite des Paddles beruehrt
+	 * @param p Eine Variable des Datentyps Paddle
+	 * @return paddle getroffen (true) oder nicht getroffen (false)
 	 */
 	public boolean hitsPaddle (Paddle p) {
 		
@@ -133,7 +142,7 @@ public class Ball {
 		{
 			return true;
 		}
-		// Ball trifft das Paddle nicht
+		// Paddle wurde nicht getroffen
 		else {
 			return false;
 		}
@@ -149,25 +158,26 @@ public class Ball {
 		// wenn das paddle getrofen wurde
 		if (hitsPaddle(p)) {
 			
-		// Ball Mittelpunkt (Endpunkt)
+		// Ball hat die Mitte an ihrem Endpunkt getroffen.
 		Position ballMittel = new Position (position.getX() + Constants.BALL_DIAMETER / 2,
 				position.getY() + Constants.BALL_DIAMETER / 2);
 		
-		//Paddle Mittelpunkt (Startpunkt)
+		// Ball hat die Mitte an ihrem Startpunkt getroffen.
 		Position paddleMittel = new Position(p.getPosition().getX() + Constants.PADDLE_WIDTH / 2,
 				p.getPosition().getY() + Constants.REFLECTION_OFFSET);
 		
-		// berechnet die ball richtung nachdem er auf paddle trifft
-		// benutzt dafuer den paddleMittelpunkt und ballMittelpunkt
+		// berechnet neue Bewegungsrichtung des Balls,
+		// nachdem er auf das Paddel trifft;
+		// benutzt dafuer paddleMittelpunkt und ballMittelpunkt
 		Vector2D newDirection = new Vector2D(paddleMittel, ballMittel);
 		direction = newDirection;
-		// Normieren der Richtung mit anpassung der Geschwindigkeit
+		// Richtung einstellen und Geschwindigkeit anpassen
 		direction.rescale();
 		}
 	}
 	
 	/**
-	 * Methode um zu uberpruefen ob der Ball einen Stein in der Matrix getroffen hat
+	 * Methode um zu uberpruefen, ob der Ball einen Stein in der Matrix getroffen hat
 	 * @param stones uebergeben eines Steinarrays
 	 * @return true wenn ein stein getroffen wird
 	 */
@@ -199,8 +209,8 @@ public class Ball {
 	}
 	/**
 	 * Methode um die Reaktion des Balls beim Auftreffen auf die Steine festzulegen
-	 * @param ballRect Eine Variable des Datentyp Rectangle
-	 * @param stoneRect Eine Variable des Datentyp Rectangle
+	 * @param ballRect Eine Variable des Datentyps Rectangle
+	 * @param stoneRect Eine Variable des Datentyps Rectangle
 	 */
 	private void reflectOnStone(Rectangle ballRect, Rectangle stoneRect) {
 		
@@ -217,8 +227,8 @@ public class Ball {
 	}
 	
 	/**
-	 * prueft ob ob der ball verloren geht 
-	 * @return true wenn der ball verloren geht 
+	 * prueft, ob ob der Ball verloren geht 
+	 * @return true wenn der Ball verloren geht 
 	 */
 	public boolean ballLost() {
 		if (position.getY() >= Constants.SCREEN_HEIGHT - Constants.BALL_DIAMETER) {
